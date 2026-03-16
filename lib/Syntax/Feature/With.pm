2d065568be9046a901ee6206b77afbe2d8b59a2c
Nigel Horne <njh@bandsman.co.uk>
2026-03-15 20:04:44 -0400

    More like "with"

--- lib/Syntax/Feature/With.pm
+++ lib/Syntax/Feature/With.pm
@@ -3,6 +3,7 @@ package Syntax::Feature::With;
 use strict;
 use warnings;
 use Exporter 'import';
+use PadWalker qw(closed_over set_closed_over);
 
 our @EXPORT_OK = qw(with);
 our $VERSION = '0.01';
@@ -11,46 +12,199 @@ our $VERSION = '0.01';
 
 Syntax::Feature::With - Simulate Pascal's "with" statement in Perl
 
+=head1 NAME
+
+Syntax::Feature::With - Lightweight lexical aliasing into a coderef using PadWalker
+
 =head1 SYNOPSIS
 
     use Syntax::Feature::With qw(with);
 
-    my $person = {
-        Name => 'Alice',
-        Age  => 30
-    };
+    my %h = ( a => 'b', x => 42 );
 
-    with($person, sub {
-        my %fields = @_;
-        print "Name: $fields{Name}\n";
-        print "Age: $fields{Age}\n";
+    # Lexicals must be declared in the outer scope
+    my ($a, $x);
+
+    with(\%h, sub {
+        print "$a\n";   # prints "b"
+        print "$x\n";   # prints "42"
+
+        $a = 'changed'; # writes back into %h
     });
+
+    say $h{a};          # "changed"
 
 =head1 DESCRIPTION
 
-This module provides a C<with> function similar to Pascal's C<with> statement.
-It allows you to pass a hash reference and a code block, and inside the block
-you can access the hash's keys as if they were local variables.
+C<with()> provides a simple, predictable way to temporarily alias hash
+keys into lexical variables inside a coderef. It is intentionally small,
+pure-Perl, and implemented using L<PadWalker>.
 
-=head1 FUNCTIONS
+This module does B<not> introduce new syntax. Instead, it gives you a
+clean functional interface:
 
-=head2 with( \%hash, sub { ... } )
+    with(\%hash, sub { ... });
 
-Executes the given code block with the hash's key-value pairs passed in as
-arguments.
+Inside the coderef, selected lexicals become read/write aliases to the
+corresponding hash entries.
+
+=head1 HOW IT WORKS
+
+C<with()> inspects the coderef's lexical pad using PadWalker. For each
+valid hash key:
+
+=over 4
+
+=item *
+
+If a lexical of the same name (e.g. C<$a> for key C<a>) exists in the
+coderef's closure pad, it is replaced with a reference to the hash slot.
+
+=item *
+
+Assignments to that lexical write back into the hash.
+
+=item *
+
+Reads from that lexical read from the hash.
+
+=back
+
+Because PadWalker can only modify lexicals that already exist in the
+coderef's closure pad, you must declare the lexicals in the outer scope:
+
+    my ($a, $b);
+    with(\%h, sub { ... });
+
+Declaring them inside the coderef will not work.
+
+=head1 USAGE
+
+=head2 with(\%hash, sub { ... })
+
+The first argument must be a hash reference.  
+The second argument must be a coderef.
+
+Example:
+
+    my %h = ( foo => 1, bar => 2 );
+    my ($foo, $bar);
+
+    with(\%h, sub {
+        $foo++;     # updates $h{foo}
+        $bar = 99;  # updates $h{bar}
+    });
+
+=head1 VALID IDENTIFIERS
+
+Only hash keys matching:
+
+    /^[A-Za-z_]\w*$/
+
+are considered valid Perl identifiers and eligible for aliasing.
+
+All others are silently ignored.
+
+=head1 UNDECLARED LEXICALS
+
+If a lexical is not declared in the outer scope, it is B<not> created or
+aliased. It simply remains C<undef> inside the coderef.
+
+Example:
+
+    my %h = ( a => 123 );
+
+    with(\%h, sub {
+        say $a;   # undef, not aliased
+    });
+
+This behaviour is intentional and avoids surprising magic.
+
+=head1 RETURN VALUE
+
+C<with()> returns whatever the coderef returns.
+
+=head1 ERROR HANDLING
+
+C<with()> will throw an exception if:
+
+=over 4
+
+=item *
+
+The first argument is not a hash reference.
+
+=item *
+
+The second argument is not a coderef.
+
+=back
+
+All other behaviour is strict-mode friendly and predictable.
+
+=head1 LIMITATIONS
+
+This module is intentionally simple. It does B<not>:
+
+=over 4
+
+=item *
+
+create new lexicals inside the coderef
+
+=item *
+
+alias array elements or object attributes
+
+=item *
+
+introduce new syntax
+
+=item *
+
+rewrite Perl code or manipulate the optree
+
+=back
+
+If you need deeper integration (keywords, syntax, chained method
+aliasing, etc.), you will need an XS-based keyword module.
+
 
 =cut
 
 sub with {
-	my ($hash_ref, $code_ref) = @_;
+    my ($hashref, $code) = @_;
 
-	# Validate arguments
-	die 'First argument must be a hash reference' unless ref($hash_ref) eq 'HASH';
-	die 'Second argument must be a code reference' unless ref($code_ref) eq 'CODE';
+    die "with: first argument must be a hashref"
+        unless ref($hashref) eq 'HASH';
 
-	# Pass hash key-value pairs to the code block
-	return $code_ref->(%{$hash_ref});
+    die "with: second argument must be a coderef"
+        unless ref($code) eq 'CODE';
+
+    # Get the coderef’s lexical pad
+    my $closed = closed_over($code);
+
+    # Copy it so we can modify it
+    my %new = %$closed;
+
+    # For each valid key, alias the lexical
+    for my $key (keys %$hashref) {
+        next unless $key =~ /^[A-Za-z_]\w*$/;   # valid Perl identifier
+        my $var = '$' . $key;
+        next unless exists $new{$var};          # only alias declared lexicals
+        $new{$var} = \$hashref->{$key};         # alias
+    }
+
+    # Install the modified pad
+    set_closed_over($code, \%new);
+
+    # Execute the coderef
+    return $code->();
 }
+
+=head1 SEE ALSO
+
+L<PadWalker>, L<Sub::Util>, L<Lexical::Alias>
 
 =head1 AUTHOR
 
--- t/with.t
+++ t/with.t
@@ -1,17 +1,153 @@
+#!/usr/bin/perl
+
 use strict;
 use warnings;
-use Test::More tests => 2;
-use lib 'lib';
+use Test::More;
+
+# We are testing a PadWalker-based implementation of:
+#
+#   with(\%hash, sub { ... });
+#
+# Contract:
+#   - First arg: hashref
+#   - Second arg: coderef
+#   - Lexicals to be aliased (e.g. $a, $x) must be declared
+#     in the *outer* scope and closed over by the coderef.
+#
+# Example:
+#   my %h = ( a => 'b' );
+#   my ($a);
+#   with(\%h, sub { print $a });   # $a aliases $h{a}
+#
+# This is required because PadWalker::closed_over only sees
+# lexicals that are closed over, not those declared inside
+# the coderef itself.
+
 use Syntax::Feature::With qw(with);
 
-my $data = { foo => 'bar', num => 42 };
-my ($foo, $num);
+# ----------------------------------------------------------------------
+# Basic aliasing: $a should alias $h{a}
+# ----------------------------------------------------------------------
 
-with($data, sub {
-    my %f = @_;
-    $foo = $f{foo};
-    $num = $f{num};
-});
+{
+    my %h = ( a => 'b' );
 
-is($foo, 'bar', 'foo field correct');
-is($num, 42, 'num field correct');
+    # Declare the lexical in the outer scope so the coderef closes over it.
+    my ($a);
+
+    my $result = with(\%h, sub {
+        # $a is *not* declared here; it is closed over from outside.
+        return $a;
+    });
+
+    is($result, 'b', 'basic aliasing: $a aliases $h{a}');
+}
+
+# ----------------------------------------------------------------------
+# Read/write aliasing: assignments should write back into the hash
+# ----------------------------------------------------------------------
+
+{
+    my %h = ( x => 10, y => 20 );
+
+    my ($x, $y);
+
+    with(\%h, sub {
+        $x += 5;   # should update $h{x}
+        $y = 99;   # should update $h{y}
+    });
+
+    is($h{x}, 15, 'writeback: $x += 5 updates $h{x}');
+    is($h{y}, 99, 'writeback: $y = 99 updates $h{y}');
+}
+
+# ----------------------------------------------------------------------
+# Only valid Perl identifiers should be aliased
+# Keys like "1abc" or "foo-bar" should be ignored
+# ----------------------------------------------------------------------
+
+{
+    my %h = (
+        good      => 'ok',
+        '1bad'    => 'no',
+        'foo-bar' => 'nope',
+    );
+
+    my ($good);   # only this one is a valid identifier
+
+    my $seen;
+
+    with(\%h, sub {
+        $seen = $good;
+    });
+
+    is($seen, 'ok', 'only valid identifiers are aliased');
+}
+
+# ----------------------------------------------------------------------
+# Undeclared lexicals should NOT be auto-created or aliased.
+# They simply remain undef.
+# ----------------------------------------------------------------------
+
+{
+    my %h = ( a => 123 );
+
+    my $value;
+
+    with(\%h, sub {
+        # $a is not declared anywhere
+        # It should remain undef
+        $value = $a;
+    });
+
+    ok(!defined $value, 'undeclared lexical remains undef (not auto-created)');
+}
+
+# ----------------------------------------------------------------------
+# Return value: with() should return whatever the coderef returns
+# ----------------------------------------------------------------------
+
+{
+    my %h = ( a => 5, b => 7 );
+
+    my ($a, $b);
+
+    my $sum = with(\%h, sub {
+        return $a + $b;
+    });
+
+    is($sum, 12, 'with() returns the coderef result');
+}
+
+# ----------------------------------------------------------------------
+# Error handling: first argument must be a hashref
+# ----------------------------------------------------------------------
+
+{
+    my $error;
+
+    eval { with([], sub {}) };
+    $error = $@;
+
+    like($error, qr/hashref/, 'dies if first argument is not a hashref');
+}
+
+# ----------------------------------------------------------------------
+# Error handling: second argument must be a coderef
+# ----------------------------------------------------------------------
+
+{
+    my %h = ( a => 1 );
+    my $error;
+
+    eval { with(\%h, "not a coderef") };
+    $error = $@;
+
+    like($error, qr/coderef/, 'dies if second argument is not a coderef');
+}
+
+# ----------------------------------------------------------------------
+# Done
+# ----------------------------------------------------------------------
+
+done_testing();
